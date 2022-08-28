c17967e1f47864e0c16a36a3019fb3b8729b719a
dev-abdal <20.abdal.03@gmail.com>
2022-08-28 22:00:48 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,279 @@
+// Program NO.1
+/* Comparing two numbers and printing the greater one. */
+
+// var numberOne:any = prompt('Enter number one')
+// var numberTwo:any = prompt('Enter number two')
+// if(numberOne > numberTwo)
+// {
+//     console.log(numberOne , 'is greater than ' , numberTwo);
+// }
+// else if(numberTwo > numberOne){
+//     console.log(numberTwo , 'is greater than ' , numberOne);   
+// }
+// else{
+//     console.log('equal or error');
+// }
+
+// Progra No.2
+/* Comparing three numbers and printing the greater one. */
+
+// var numberOne:any = prompt('Enter number one')
+// var numberTwo:any = prompt('Enter number two')
+// var numberThree:any = prompt('Enter number three')
+// if(numberOne > numberTwo && numberOne > numberThree) 
+// {
+//     console.log(numberOne , 'is greater than other numbers');
+// }
+// else if(numberTwo > numberOne && numberTwo > numberThree)
+// {
+//     console.log(numberTwo , 'is greater than other numbers');
+// }
+// else if(numberThree > numberOne && numberThree > numberOne)
+// {
+//     console.log(numberThree , 'is greater than other numbers');
+// }
+// else{
+//     console.log('equals / error');
+// }
+
+
+// Program No.3
+ /* Asking the user to enter a number and then it is checking if the number is positive, negative or
+ zero. */
+//   var inputNumber :any = prompt('enter number to find negative , positive or zero number') 
+//  if(inputNumber > 0)
+// {
+//     console.log('Positive number'); 
+// }
+// else if(inputNumber < 0 )
+// {
+//     console.log('Negative');
+// }
+// else{
+//     console.log('Zero');
+// }
+
+// Program No.4
+  /* Asking the user to enter a number and then it is checking if the number is divisible by 5 and 11. */
+//     var inputOneNumber:any = prompt("Enter the number ")
+//        if(inputOneNumber % 5 == 0  && inputOneNumber % 11 == 0)
+//           {
+//               console.log(inputOneNumber , 'This number is divisible by 5 and 11');
+//           }
+//          else{
+//               console.log(inputOneNumber , 'This number is not divisible by 5 and 11');
+//          }
+
+
+// Program NO.5
+/* A TypeScript program to check whether a number is even or odd. */
+// var valueOfUser: any = prompt("enter number to check even or odd")
+// if (valueOfUser % 2 == 0) {
+//     console.log(valueOfUser, 'is even');
+// }
+// else {
+//     console.log(valueOfUser, 'is odd');
+// }
+
+// Program No.6
+/* The above code is checking whether the input year is leap year or not. */
+//  var inputLeap : any = prompt("Enter value to find leap year")
+//  if(inputLeap % 4 == 0 )
+//  {
+//        console.log(inputLeap , 'this is leap year');
+//  }
+//  else{
+//        console.log(inputLeap, 'is not a leap year');
+//  }
+       
+// Program No.7
+// var iC : any = prompt("enter charactor ")
+// if( iC >= 'a' && iC <= 'z' || iC >= 'A' && iC <= 'Z' )
+// {
+//        console.log(iC, ' is alphabet');
+// }
+// else{
+//        console.log(iC , ' is not alphabet');      
+// }
+
+
+ // Program No.8
+//  var enteralph: any = prompt(' Enter the alphabet')  ;
+//  var loweralpha:any = enteralph == 'a' || enteralph == 'e' || enteralph == 'i' || enteralph == 'o' || enteralph == 'u'
+//  var upperalpha:any = (enteralph == 'A' || enteralph == 'E' || enteralph == 'I' || enteralph == 'O' || enteralph == 'U')
+//  if( enteralph && (loweralpha || upperalpha) )
+//  {
+//        console.log(enteralph , ' is vowel');
+//  }
+//  else{
+//        console.log(enteralph , ' is consonent');
+//  } 
+
+        
+ // Program No.9
+//   var alphaInput : any = prompt(" Enter the value to check alpha or number and symbol ")      
+//    if( alphaInput >= 'a' && alphaInput <= 'z' || alphaInput >= 'A' && alphaInput <='Z' )
+//    {
+//        console.log(alphaInput , 'is alphabet');
+//    }
+//    else if(alphaInput <=0 || alphaInput >=0)
+//    {
+//        console.log(alphaInput , 'is number');
+//    }
+//    else{
+//        console.log(alphaInput , 'is special character');
+//    }
+  
+
+ // Program No.10
+//  var alphha : any = prompt(" Enter the alphabet")
+//  var loweralphha : any = (alphha >= 'a' || alphha <= ' z')
+//  if(loweralphha){
+//        console.log(alphha , 'is lowercase');
+//  }
+//  else{
+//        console.log(alphha , ' is uppercase');
+//  }
+   
+
+// Program No.11
+// var weekNo:any = prompt("Enter week number")
+// if(weekNo == 1)
+// {
+//        console.log("Monday");
+// }
+// else if(weekNo == 2)
+// {
+//        console.log("Tuesday");
+// }
+// else if(weekNo == 3)
+// {
+//        console.log("Wednesday");
+// }
+// else if(weekNo == 4)
+// {
+//        console.log("Thursday");
+// }
+// else if(weekNo == 5)
+// {
+//        console.log("Friday");
+// }
+// else if(weekNo == 6)
+// {
+//        console.log("Saturday");
+// }
+// else if(weekNo == 7)
+// {
+//        console.log("Sunday");
+// }
+// else{
+//        console.log('Error');
+// }
+
+               
+//Program 12
+
+// var monthNo : any = prompt("Enter the number of month") 
+// if(monthNo == 1 || monthNo == 3 || monthNo == 5 || monthNo == 7 || monthNo == 8 || monthNo == 10 || monthNo == 12 ) 
+// {
+//       console.log('31 days in this month');
+// }
+// else if(monthNo == 4 || monthNo == 6 || monthNo == 9 || monthNo == 11)
+// {
+//        console.log(' 30 days in this month');
+// }
+// else if(monthNo == 2)
+// {
+//       console.log(' 28 or 29 days in this month');
+// }
+// else{
+// console.log(' ! enter between (1-12)');
+// }
+
+
+
+
+
+// Progra No.14
+// var triLeft: any = prompt("enter left side angle of triangle")
+// var triright : any = prompt("enter right side angle of triangle")
+// var tribottom : any = prompt("enter bottom side angle of triangle")
+// if(triLeft + triright <= tribottom || triLeft + tribottom <= triright || triright + tribottom <= triLeft)
+// {
+//        console.log("trianlge is valid");
+// }
+// else{
+//        console.log("triangle is invalid");
+// }
+           
+
+
+//  // Program No.15
+//     var triOne  : any = prompt(" enter one side")
+//     var tritwo  : any = prompt(" enter second side")
+//     var trithree: any = prompt(" enter third side")
+//     if(triOne == tritwo && triOne == trithree && tritwo == trithree  )
+//     {
+//        console.log('triangle is valid');
+//     }
+//     else{
+//        console.log('triangle is not valid');
+//     }
+
+       
+
+// Program No.16
+// var sideLeft : any = prompt("enter side")
+// var sideright : any = prompt("enter side")
+// var sidebottom: any = prompt("enter side")
+// if(sideLeft == sideright && sidebottom == sideright)
+// {
+//        console.log('This is equilateral triangle ');
+// }
+// else if( sideLeft == sideright || sideLeft == sidebottom || sidebottom == sideright)
+// {
+//        console.log('This is isosceles triangle');
+// }
+// else{
+//        console.log('This is scalene  triangle');
+// }
+ 
+              
+
+// Program No.19
+// var phy: string| null=prompt("Enter number of physics")
+//  var phynum: number= Number(phy)
+// var chem : string| null  = prompt("Enter number of Chemistry")
+// var chemnum: number= Number(chem)
+// var bio: string| null = prompt("Enter number of Biology")
+// var bionum: number= Number(bio)
+// var math: string| null  = prompt("Enter number of Math")
+// var mathnum: number= Number(math)
+// var comp: string| null  = prompt("Enter number of Computer")
+// var compnum: number= Number(comp)
+// var sum : number= (phynum + chemnum + bionum + mathnum + compnum) / 5.0
+// console.log('Total marks ',sum);
+// if(sum >= 90 && sum <=100)
+// {
+//        console.log('Grade is A');
+// }
+// else if ( sum >= 80 && sum <= 88)
+// {
+//        console.log('Grade is B');
+// }
+// else if ( sum >= 70 && sum <= 79)
+// {
+//        console.log('Grade is D');
+// }
+// else if ( sum >= 60 && sum <= 69)
+// {
+//        console.log('Grade is D');
+// }
+// else if ( sum >= 40 && sum <=59 )
+// {
+//        console.log('Grade is E');
+// }
+// else{
+//        console.log('Fail');
+// }
+
